9aed91030a1b3eed0ada60b61007c60e3f9f0b4d
sushildalavi <75267758+sushildalavi@users.noreply.github.com>
2026-05-29 00:41:02 -0700

    Add query builder test for empty-term input

--- Tests/LocalMindKitCoreTests/QueryServiceTests.swift
+++ Tests/LocalMindKitCoreTests/QueryServiceTests.swift
@@ -6,4 +6,9 @@ final class QueryServiceTests: XCTestCase {
         let built = QueryService.buildMatchQuery(from: "apple  \"job\"   link")
         XCTAssertEqual(built, "\"apple\" AND \"job\" AND \"link\"")
     }
+
+    func testBuildMatchQueryDropsEmptyTerms() {
+        let built = QueryService.buildMatchQuery(from: "   \"\"   ")
+        XCTAssertEqual(built, "")
+    }
 }
